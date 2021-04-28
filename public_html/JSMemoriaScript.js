64f7d5c7945e16cfdce933352330e88d193edc9a
RolanHun <kisbartok01@gmail.com>
2021-04-28 09:44:40 +0200

    DefaultStart

---
+++ public_html/JSMemoriaScript.js
@@ -0,0 +1,74 @@
+$(function(){
+    $("div").append("<img>");
+    $("div img").eq(0).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(0).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(1).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(1).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(2).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(2).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(3).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(3).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(4).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(4).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(5).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(5).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(6).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(6).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(7).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(7).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(8).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(8).attr("alt",kepek[0].alt);0
+    $("div").append("<img>");
+    $("div img").eq(9).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(9).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(10).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(10).attr("alt",kepek[0].alt);
+    $("div").append("<img>");
+    $("div img").eq(11).attr("src",kepek[0].eleresiUt);
+    $("div img").eq(11).attr("alt",kepek[0].alt);
+});
+
+var kep1={
+    eleresiUt:"pics/start.jpg",
+    alt:"start"
+};
+var kep2={
+    eleresiUt:"pics/wrong.jpg",
+    alt:"wrong"
+};
+var kep3={
+    eleresiUt:"pics/bmw.jpg",
+    alt:"BMW"
+};
+var kep4={
+    eleresiUt:"pics/r8.jpg",
+    alt:"R8"
+};
+var kep5={
+    eleresiUt:"pics/488.jpg",
+    alt:"488"
+};
+var kep6={
+    eleresiUt:"pics/c63s.jpg",
+    alt:"C63S"
+};
+var kep7={
+    eleresiUt:"pics/huracan.jpg",
+    alt:"Huracan"
+};
+var kep8={
+    eleresiUt:"pics/supra.jpg",
+    alt:"Supra"
+};
+
+
+var kepek=[kep1,kep2,kep3,kep4,kep5,kep6,kep7,kep8];
